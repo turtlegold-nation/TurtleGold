eb440c699c513743fad65b3671d10f0b4ff05577
Learner <linhdqtg@gmail.com>
2019-07-10 03:39:59 +0700

    New nation
    Capital hehe

---
+++ src/config/Ascii.h
@@ -0,0 +1,48 @@
+// Copyright (c) 2018, The TurtleCoin Developers
+// Copyright (c) 2018, The DeroGold Association
+// Copyright (c) 2019, The TurtleGold Developers
+// Please see the included LICENSE file for more information
+
+#pragma once
+
+const std::string asciiArt =
+"                                                                       \n"
+"                                      N  N                             \n"
+"                                    hsoooosh                           \n"
+"                               Ndyooooooooooooyd                       \n"
+"                           NdhsoooooooooooooooooosydN                  \n"
+"                           hooooooooooooooooooooooooy                  \n"
+"                    Ndys    hooooooooooooooooooooooh   NsydN           \n"
+"                Ndysooooo    do+++++++++++++++++ooh    ooooosydN       \n"
+"              hsoooooooo+oh    o++++++++++++++++od   doooooooooosh     \n"
+"          Nyoooooooooo+++++y   No++++++++++++++o    h++oooooooooooooyN \n"
+"          hooooooooo++++++++s   NyooooooooooooyN   y+++++ooooooooooooy \n"
+"          dooooooo+++++++++++s                    y++++++++ooooooooooh \n"
+"           ooooo+++++++++++++y    NNNNNNNNNNNN    h+++++++++oooooooood \n"
+"          Noooo+++++++++++++h    o////////////+d   d++++++++++ooooooo  \n"
+"           ooo+++++++++++/+d   d////////////////h    ++++++++++ooooooN \n"
+"           yo+++++++++++/+    h//////////////////y    o+++++++++oooos  \n"
+"            o++++++++++/o    y////////////////////s   No++++++++ooood  \n"
+"            dddddddddd     s//////::::::://///////oN    ddddddddd      \n"
+"                            /////:::::::::://///////d                  \n"
+"             hyssssssssy    d///::::::::::::///////h   Nyssssyyyyyy    \n"
+"              ++++++++///h   d///::::::::::://////d   d++++++++++od    \n"
+"              y+++++++////y    +/:::::::::://////    h+++++++++++s     \n"
+"               o++++++/////s   N+//::::::://///+N   y/++++++++++oN     \n"
+"               No++++++/////oN   yo++++++++++oyN   s//+++++++++o       \n"
+"                 +++++++/////o                    s/+++++++++++d       \n"
+"                 d++++++++///h                    d+++++++++++d        \n"
+"                   +++++++++d   d+////////////+h    +++++++++d         \n"
+"                    o++++++    h//////////////++y    o+++++od          \n"
+"                    No+++o    y++++++++++++++++++y   No+++o            \n"
+"                      y+sN   s++++++++++++++++++++sN   y+sN            \n"
+"                           Ns++++++++++++++++++++++oN   d              \n"
+"                          No++++++++++++++++++++++++o                  \n"
+"                          doo+++++++++++++++++++oooood                 \n"
+"                            hooooooooooooooooooooooyN                  \n"
+"                             NyooooooooooooooooooyN                    \n"
+"                               Nyooooooooooooooy                       \n"
+"                                 NhooooooooooyN                        \n"
+"                                    dsoooosdN                          \n"
+"                                       dd                              \n"
+"                                                                       \n";
